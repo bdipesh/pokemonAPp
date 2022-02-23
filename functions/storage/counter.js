36660b50a2e811079b91829cfb943acf02e156b2
Dipesh Basnet <bdipesh08@gmail.com>
2022-02-23 20:40:27 +0545

    changes

--- functions/storage/counter.js
+++ functions/storage/counter.js
@@ -1,4 +1,4 @@
 export const onRequest = () => {
   const views = await KV.get('views')
-  return new Response(new Date().toISOString() + views);
+  return new Response(new Date().toISOString() + 'since in types' + await JSON.stringify({'views': views}));
 };
