aa17986e02e189e9a542b22d2b89a7d1d3e07ff3
Dipesh Basnet <bdipesh08@gmail.com>
2022-02-28 15:11:00 +0545

    new build

--- functions/api/[token]/upload.js
+++ functions/api/[token]/upload.js
@@ -1,14 +1,6 @@
-export async function onRequest(context) {
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
+export async function onRequest({ env, params }) {
   const token = await env.KV.get("token");
-  if (paramsToken === token) {
+  if (params.token === token) {
     return new Response("Success", token);
   } else {
     return new Response("failled", token);
